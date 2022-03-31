e10313bd6cceeff707a937971082822947c870b1
VitorRiedo <vitorsantos@gruposgapetro.com.br>
2022-03-31 09:38:23 -0300

    Projeto Monitor
    • Monitorando serviços especificados.

---
+++ Monitor/Monitor/Class/ServicesController.cs
@@ -0,0 +1,132 @@
+﻿using System.ServiceProcess;
+
+namespace Monitor.Class
+{
+    public class ServicesController
+    {
+        private ServiceController sc;
+        public bool IsRunMonitorService { get; set; }
+
+        public ServicesController(string nameService)
+        {
+            sc = new ServiceController(nameService);
+            IsRunMonitorService = true;
+
+            MonitorServiceAlwaysStartedAsync();
+            //MonitorServiceAlwaysStopedAsync();
+        }
+
+        private ServiceControllerStatus GetStatus
+        {
+            get { return sc.Status; }
+        }
+
+        private bool IsRunning
+        {
+            get { return GetStatus == ServiceControllerStatus.Running; }
+        }
+
+        private bool IsStartPending
+        {
+            get { return GetStatus == ServiceControllerStatus.StartPending; }
+        }
+
+        private bool IsPaused
+        {
+            get { return GetStatus == ServiceControllerStatus.Paused; }
+        }
+
+        private bool IsPausePending
+        {
+            get { return GetStatus == ServiceControllerStatus.PausePending; }
+        }
+
+        private bool IsStopped
+        {
+            get { return GetStatus == ServiceControllerStatus.Stopped; }
+        }
+
+        private bool IsStopPending
+        {
+            get { return GetStatus == ServiceControllerStatus.StopPending; }
+        }
+
+        private bool IsContinuePending
+        {
+            get { return GetStatus == ServiceControllerStatus.ContinuePending; }
+        }
+
+        private void StartService()
+        {
+            sc.Start();
+            Console.WriteLine("Iniciando Serviço: " + sc.DisplayName);
+        }
+
+        private void ContinueService()
+        {
+            sc.Continue();
+            Console.WriteLine("Continuando Serviço: " + sc.DisplayName);
+        }
+
+        private void StopService()
+        {
+            sc.Stop();
+            Console.WriteLine("Parando Serviço: " + sc.DisplayName);
+        }
+
+        private async Task MonitorServiceAlwaysStartedAsync()
+        {
+            await Task.Run(() => {
+                try
+                {
+                    while (IsRunMonitorService)
+                    {
+                        Console.WriteLine("Verificando Serviço: " + sc.DisplayName);
+
+                        if (IsStopped)
+                        {
+                            StartService();
+                        }
+                        else
+                        if (IsPaused)
+                        {
+                            ContinueService();
+                        }
+
+                        sc.Refresh();
+                        Thread.Sleep(5000);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"[{nameof(MonitorServiceAlwaysStartedAsync)}]Erro Serviço: {sc.DisplayName} - {e.Message}");
+                }
+            });
+        }
+
+        private async Task MonitorServiceAlwaysStopedAsync()
+        {
+            await Task.Run(() => {
+                try
+                {
+                    while (IsRunMonitorService)
+                    {
+                        Console.WriteLine("Verificando Serviço: " + sc.DisplayName);
+
+                        if (IsRunning)
+                        {
+                            StopService();
+                        }
+
+                        sc.Refresh();
+                        Thread.Sleep(5000);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"[{nameof(MonitorServiceAlwaysStopedAsync)}]Erro Serviço: {sc.DisplayName} - {e.Message}");
+                }
+            });
+        }
+    }
+}
---
+++ Monitor/Monitor/Program.cs
@@ -0,0 +1,102 @@
+﻿using Monitor.Class;
+using System.Diagnostics;
+using System.ServiceProcess;
+
+
+class Program
+{
+    public enum SimpleServiceCustomCommands
+    {
+        StopWorker = 128, RestartWorker, CheckWorker
+    };
+
+    static void Main(string[] args)
+    {
+        var ListServicesController = new List<ServicesController>();
+        var ListNameServices = new List<string>() { "SrvMainSgaBot", "SgaSync" };
+
+        ListNameServices.ForEach(nameService => ListServicesController.Add(new ServicesController(nameService)));
+
+        Thread thread = new Thread(() =>
+        {
+            while (ListServicesController.Any(sc => sc.IsRunMonitorService))
+            {
+                Thread.Sleep(5000);
+            }
+        });
+
+        thread.Start();
+        thread.Join();
+        //Task.Wait();
+
+        //ServiceController[] scServices;
+        //scServices = ServiceController.GetServices();
+
+        //foreach (ServiceController scTemp in scServices)
+        //{
+        //    Console.WriteLine("Service = " + scTemp.ServiceName);
+        //    if (ListNameServices.Contains(scTemp.ServiceName))
+        //    {
+        //        ServiceController sc = new ServiceController(scTemp.ServiceName);
+        //        Console.WriteLine("Status = " + sc.Status);
+        //        Console.WriteLine("Can Pause and Continue = " + sc.CanPauseAndContinue);
+        //        Console.WriteLine("Can ShutDown = " + sc.CanShutdown);
+        //        Console.WriteLine("Can Stop = " + sc.CanStop);
+        //        if (sc.Status == ServiceControllerStatus.Stopped)
+        //            if (sc.Status == ServiceControllerStatus.Stopped)
+        //            {
+        //                sc.Start();
+        //                while (sc.Status == ServiceControllerStatus.Stopped)
+        //                {
+        //                    Thread.Sleep(1000);
+        //                    sc.Refresh();
+        //                }
+        //            }
+        //        // Issue custom commands to the service
+        //        // enum SimpleServiceCustomCommands
+        //        //    { StopWorker = 128, RestartWorker, CheckWorker };
+        //        sc.ExecuteCommand((int)SimpleServiceCustomCommands.StopWorker);
+        //        sc.ExecuteCommand((int)SimpleServiceCustomCommands.RestartWorker);
+        //        sc.Pause();
+        //        while (sc.Status != ServiceControllerStatus.Paused)
+        //        {
+        //            Thread.Sleep(1000);
+        //            sc.Refresh();
+        //        }
+        //        Console.WriteLine("Status = " + sc.Status);
+        //        sc.Continue();
+        //        while (sc.Status == ServiceControllerStatus.Paused)
+        //        {
+        //            Thread.Sleep(1000);
+        //            sc.Refresh();
+        //        }
+        //        Console.WriteLine("Status = " + sc.Status);
+        //        sc.Stop();
+        //        while (sc.Status != ServiceControllerStatus.Stopped)
+        //        {
+        //            Thread.Sleep(1000);
+        //            sc.Refresh();
+        //        }
+        //        Console.WriteLine("Status = " + sc.Status);
+        //        String[] argArray = new string[] { "ServiceController arg1", "ServiceController arg2" };
+        //        sc.Start(argArray);
+        //        while (sc.Status == ServiceControllerStatus.Stopped)
+        //        {
+        //            Thread.Sleep(1000);
+        //            sc.Refresh();
+        //        }
+        //        Console.WriteLine("Status = " + sc.Status);
+        //        // Display the event log entries for the custom commands
+        //        // and the start arguments.
+        //        EventLog el = new EventLog("Application");
+        //        EventLogEntryCollection elec = el.Entries;
+        //        foreach (EventLogEntry ele in elec)
+        //        {
+        //            if (ele.Source.IndexOf("SimpleService.OnCustomCommand") >= 0 |
+        //                ele.Source.IndexOf("SimpleService.Arguments") >= 0)
+        //                Console.WriteLine(ele.Message);
+        //        }
+        //    }
+        //}
+    }
+}
